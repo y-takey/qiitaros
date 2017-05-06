19fed35ff5804d1b3f49164a53d149bbc0792760
takeyuichi <y.takey@gmail.com>
2017-05-06 11:52:49 +0900

    Show github calendar

--- src/DatePage.jsx
+++ src/DatePage.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
+import HeatCalendar from 'react-heat-calendar';
 import Page from './Page';
 
 export default class DatePage extends React.Component {
   render() {
     return (
       <Page title="日時から見るQiita" {...this.props}>
-        hoge
+        <HeatCalendar
+          beginDate={new Date('2017-01-01')}
+          endDate={new Date('2017-12-31')}
+          data={this.props.data}
+          dateField="created_at"
+        />
       </Page>
     )
   }
--- src/app.jsx
+++ src/app.jsx
@@ -7,7 +7,7 @@ import TagPage from './TagPage';
 import CharPage from './CharPage';
 import OtherPage from './OtherPage';
 
-const json = require('../data/tag_summary_2017-03-01_2017-04-30.json');
+const data = require('../data/post_summary_2017-03-01_2017-04-30.json');
 const menu = { date: "日時", user: "ユーザ", tag: "タグ", char: "文字数", other: "その他" }
 const oddContentStyle = {
   backgroundColor: "#ffffff"
@@ -21,11 +21,11 @@ export default class App extends React.Component {
         <main className="mdl-layout__content">
           <div className="page-content">
             <a name="top"></a>
-            <DatePage name="date" next="user" extStyle={oddContentStyle}/>
-            <UserPage name="user" next="tag" prev="date"/>
-            <TagPage name="tag" next="char" prev="user" extStyle={oddContentStyle}/>
-            <CharPage name="char" next="other" prev="tag"/>
-            <OtherPage name="other"  prev="char" extStyle={oddContentStyle}/>
+            <DatePage data={data} name="date" next="user" extStyle={oddContentStyle}/>
+            <UserPage data={data} name="user" next="tag" prev="date"/>
+            <TagPage data={data} name="tag" next="char" prev="user" extStyle={oddContentStyle}/>
+            <CharPage data={data} name="char" next="other" prev="tag"/>
+            <OtherPage data={data} name="other"  prev="char" extStyle={oddContentStyle}/>
           </div>
 
           <Footer />
